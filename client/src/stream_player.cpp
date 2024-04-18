d0ebf3a87c4963e9547ca63ceb6d3914bac2e35e
李自然 <liziran2@huawei.com>
2024-04-18 01:51:16 +0000

    64位适配
    Signed-off-by: 李自然 <liziran2@huawei.com>

--- client/src/stream_player.cpp
+++ client/src/stream_player.cpp
@@ -61,7 +61,6 @@ int32_t StreamPlayer::SetSurface(const std::string &surfaceId)
 
     sptr<Surface> surface = SurfaceUtils::GetInstance()->GetSurface(surfaceUniqueId);
     if (!surface) {
-        CLOGE("surface is null, surface uniqueId %llu", surfaceUniqueId);
         return CAST_ENGINE_ERROR;
     }
     sptr<IBufferProducer> producer = surface->GetProducer();
