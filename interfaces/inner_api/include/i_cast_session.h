fdbd8fbf375bc327e30100421781bb52d0f6d733
winson <winson.wang@huawei.com>
2025-02-24 02:29:55 +0000

    update interfaces/inner_api/include/i_cast_session.h.
    Signed-off-by: winson <winson.wang@huawei.com>

--- interfaces/inner_api/include/i_cast_session.h
+++ interfaces/inner_api/include/i_cast_session.h
@@ -52,10 +52,7 @@ public:
     virtual int32_t RegisterListener(std::shared_ptr<ICastSessionListener> listener) = 0;
     virtual int32_t UnregisterListener() = 0;
     virtual int32_t AddDevice(const CastRemoteDevice &remoteDevice) = 0;
-    virtual int32_t RemoveDevice(const std::string &deviceId, const int32_t &type)
-    {
-        return 0;
-    }
+    virtual int32_t RemoveDevice(const std::string &deviceId, const int32_t &type) = 0;
     virtual int32_t RemoveDevice(const std::string &deviceId,
         const DeviceRemoveAction &actionType = DeviceRemoveAction::ACTION_DISCONNECT) = 0;
     virtual int32_t StartAuth(const AuthInfo &authInfo) = 0;
