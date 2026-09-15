48a99b654bc6b78365d7f1690974326cdf77d242
shukz <shukaizhong@h-partners.com>
2026-09-15 16:17:58 +0800

    modify json object
    Signed-off-by: shukz <shukaizhong@h-partners.com>

--- service/src/session/src/stream/src/cast_stream_manager_client.cpp
+++ service/src/session/src/stream/src/cast_stream_manager_client.cpp
@@ -291,8 +291,8 @@ bool CastStreamManagerClient::NotifyPeerKeyResponse(const std::string &mediaId, 
 bool CastStreamManagerClient::NotifyPeerMediaInfoChanged(const MediaInfo &mediaInfo)
 {
     CLOGD("NotifyPeerMediaInfoChanged in");
-    CastJsonObject body(false);
-    EncapMediaInfo(mediaInfo, body, IsDoubleFrame());
+    json info;
+    EncapMediaInfo(mediaInfo, info, IsDoubleFrame());
     return SendControlAction(ACTION_MEDIA_INFO_CHANGED, std::move(body));
 }
 
